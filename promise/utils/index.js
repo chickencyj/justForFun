13fcd4ba4029d1e1b742556a88796fddcd628402
yujun <yujun.chen@implustech.com>
2017-09-27 16:09:41 +0800

    promiseDemo

---
+++ promise/utils/index.js
@@ -0,0 +1,7 @@
+const toStringEqual = (obj, str) => Object.toString.call(obj) === str
+
+const isFunction = obj => toStringEqual(obj, '[object Function]')
+
+const isArray = obj => toStringEqual(obj, '[object Array]')
+
+module.exports = { isFunction, isArray }
